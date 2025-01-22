d01300925fcebdf9320b2f6f2797285887dc5884
Kiyalan <kiyyaforever007@gmail.com>
2025-01-23 01:50:23 +0800

    Tweak window setting: forward-compatible, MSAA
    1. Add forward-compatible for OpenGL
    2. Set the multi-sampling anti-aliasing to the maximum

--- code/CoreEngineLayer/src/window/window.cpp
+++ code/CoreEngineLayer/src/window/window.cpp
@@ -1,23 +1,36 @@
+#include <glad/glad.h>
 #include <window/window.hpp>
+#include <utility.hpp>
+/*
+    future: windowtune
+*/
 
 namespace Engine {
 
 Window::Window(WindowSetting iWindowSetting) :_windowSetting{iWindowSetting} {
-    _bWindowInitialized = 0;
-    _windowHandle = nullptr;
+
 }
 
 Window::~Window(){
-    
+    if (_bWindowInitialized)
+        Destroy();
 }
 
 bool Window::Initialize(){
-    if (_bWindowInitialized)
+    if (_bWindowInitialized) {
+        PRINT_ERROR("Window already initialized");
         return 0;
+    }
+    PRINT_LOG("Window initialization begins.");
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, true);
     glfwWindowHint(GLFW_RESIZABLE, false);
+    // Employ the max multi-sampling anti-aliasing
+    FInt maxSamples;
+    glGetIntegerv(GL_MAX_SAMPLES, &maxSamples);
+    glfwWindowHint(GLFW_SAMPLES, maxSamples);
     // Do not support multi-monitor 
     switch (static_cast<int>(_windowSetting.displayState)) {
         case static_cast<int>(EWindowDisplayState::WindowedFullScreen): {
@@ -36,20 +49,28 @@ bool Window::Initialize(){
             _windowHandle = glfwCreateWindow(_windowSetting.windowWidth, _windowSetting.windowHeight, _windowSetting.windowTitle.c_str(), nullptr, nullptr);
             break;
     }
-    _bWindowInitialized = 1;
-    return 1;
+    if (_windowHandle) {
+        PRINT_LOG("Window successfully initialized.");
+        _bWindowInitialized = 1;
+        return 1;
+    } else {
+        PRINT_ERROR("Window initialzation failed.");
+        return 0;
+    }
 }
 
 void Window::Destroy(){
     if (_bWindowInitialized) {
         glfwDestroyWindow(_windowHandle);
         _bWindowInitialized = 0;
+    } else {
+        PRINT_ERROR("Window cannot be destroyed before initialized.");
     }
 }
 
 bool Window::ShouldWindowClose() const {
     if (!_bWindowInitialized) {
-        // Throw a exception because the window hasn't been initialized
+        PRINT_ERROR("Window cannot be used before initialized.");
     }
     return glfwWindowShouldClose(_windowHandle);
 }
